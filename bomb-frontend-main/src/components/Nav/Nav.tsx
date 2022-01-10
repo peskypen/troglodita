cc310b5e27b0a421d3078d4b33f03c7660641994
pesky <ottorokosama@gmail.com>
2022-01-10 20:16:16 +0100

    .

---
+++ bomb-frontend-main/src/components/Nav/Nav.tsx
@@ -0,0 +1,243 @@
+import React, {useMemo} from 'react';
+import clsx from 'clsx';
+import {Link} from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+} from '@material-ui/core';
+
+import ListItemLink from '../ListItemLink';
+import useBombStats from '../../hooks/useBombStats';
+import useBtcStats from '../../hooks/useBtcStats';
+import useShareStats from '../../hooks/usebShareStats';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import AccountButton from './AccountButton';
+
+import bombLogo from '../../assets/img/bomb-logo.png';
+import {roundAndFormatNumber} from '../../0x';
+//import TokenSymbol from '../TokenSymbol';
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    color: '#f9d749',
+    'background-color': '#171923',
+    // borderBottom: `1px solid ${theme.palette.divider}`,
+    padding: '10px',
+    marginBottom: '3rem',
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+  hide: {
+    display: 'none',
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    fontFamily: 'Rubik',
+    fontSize: '0px',
+    flexGrow: 1,
+  },
+  link: {
+    textTransform: 'uppercase',
+    color: '#f9d749',
+    fontSize: '18px',
+    marginTop: '15px',
+    margin: theme.spacing(10, 1, 1, 2),
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+  brandLink: {
+    textDecoration: 'none',
+    color: '#f9d749',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+}));
+
+const Nav = () => {
+  const matches = useMediaQuery('(min-width:900px)');
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const bombStats = useBombStats();
+  const btcStats = useBtcStats();
+  const shareStats = useShareStats();
+
+ // const [connected, setConnected] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const btcPriceInDollars = useMemo(() => (btcStats ? Number(btcStats).toFixed(2) : null), [btcStats]);
+  const bombPriceInDollars = useMemo(
+    () => (bombStats ? Number(bombStats.priceInDollars).toFixed(2) : null),
+    [bombStats],
+  );
+  const sharePriceInDollars = useMemo(
+    () => (shareStats ? Number(shareStats.priceInDollars).toFixed(2) : null),
+    [shareStats],
+  );
+
+  return (
+    <AppBar position="sticky" elevation={0} className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        {matches ? (
+          <>
+            <Typography variant="h6" color="inherit" noWrap style={{flexGrow: '0'}} className={classes.toolbarTitle}>
+              {/* <a className={ classes.brandLink } href="/">Bomb Money</a> */}
+              <Link to="/" color="inherit" className={classes.brandLink}>
+                <img alt="bomb.money" src={bombLogo} height="60px" />
+              </Link>
+            </Typography>
+            <Box style={{paddingLeft: '15px', paddingTop: '10px', fontSize: '1rem', flexGrow: '1'}}>
+              <Link to="/" className={'navLink ' + classes.link}>
+                Home
+              </Link>
+              <Link to="/farm" className={'navLink ' + classes.link}>
+                Farm
+              </Link>
+              <Link to="/boardroom" className={'navLink ' + classes.link}>
+                Blacksmith
+              </Link>
+              <Link to="/bond" className={'navLink ' + classes.link}>
+                Bond
+              </Link>
+
+              {/* <Link color="textPrimary" to="/sbs" className={classes.link}>
+                SBS
+              </Link>
+              <Link color="textPrimary" to="/liquidity" className={classes.link}>
+                Liquidity
+              </Link>
+              <Link color="textPrimary" to="/regulations" className={classes.link}>
+                Regulations
+              </Link> */}
+              <a href="https://docs.bomb.money" className={'navLink ' + classes.link} rel="noopener noreferrer" target="_blank">
+                Docs
+              </a>
+                {/* <a href="https://bomb.farm" className={'navLink ' + classes.link} rel="noopener noreferrer" target="_blank">
+                AutoVaults
+              </a> */}
+            </Box>
+
+            <Box
+              style={{
+                flexGrow: '0',
+                paddingLeft: '15px',
+                paddingTop: '5px',
+                fontSize: '1rem',
+                paddingRight: '15px',
+                height: '30px',
+                display: 'flex',
+              }}
+            >
+              <div className="navTokenIcon bomb"></div>{' '}
+              <div className="navTokenPrice">$????</div>
+              <div className="navTokenIcon bshare"></div>{' '}
+              <div className="navTokenPrice">$????</div>
+              <div className="navTokenIcon btc"></div>{' '}
+              <div className="navTokenPrice">$????</div>
+            </Box>
+            <AccountButton text="Connect wallet" />
+          </>
+        ) : (
+          <>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <img
+              alt="bomb.money"
+              src={bombLogo}
+              style={{height: '40px', marginTop: '-10px', marginLeft: '10px', marginRight: '15px'}}
+            />
+            <AccountButton text="Connect" />
+            <Drawer
+                className={classes.drawer}
+                onClose={handleDrawerClose}
+              // onEscapeKeyDown={handleDrawerClose}
+              // onBackdropClick={handleDrawerClose}
+              variant="temporary"
+              anchor="left"
+              open={open}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              <div>
+                <IconButton onClick={handleDrawerClose}>
+                  {theme.direction === 'rtl' ? (
+                    <ChevronRightIcon htmlColor="white" />
+                  ) : (
+                    <ChevronLeftIcon htmlColor="white" />
+                  )}
+                </IconButton>
+              </div>
+              <Divider />
+              <List>
+                <ListItem>
+                  <AccountButton text="Connect wallet" />
+                </ListItem>
+                <ListItemLink primary="Home" to="/" />
+                <ListItemLink primary="Farm" to="/farm" />
+                <ListItemLink primary="Blacksmith" to="/boardroom" />
+                <ListItemLink primary="Bond" to="/bond" />
+                {/* <ListItemLink primary="SBS" to="/sbs" /> */}
+                {/* <ListItemLink primary="Liquidity" to="/liquidity" /> */}
+                {/* <ListItemLink primary="Regulations" to="/regulations" /> */}
+                <ListItem button component="a" href="https://docs.bomb.money">
+                  <ListItemText>Docs</ListItemText>
+                  </ListItem>
+                 {/*   <ListItem button component="a" href="https://bomb.farm">
+                  <ListItemText>AutoVaults</ListItemText>
+                </ListItem> */}
+              </List>
+            </Drawer>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Nav;
---
+++ bomb-frontend-main/src/views/Home/Home.js
@@ -0,0 +1,409 @@
+import React, { useMemo } from 'react';
+import Page from '../../components/Page';
+import { createGlobalStyle } from 'styled-components';
+import CountUp from 'react-countup';
+import CardIcon from '../../components/CardIcon';
+import TokenSymbol from '../../components/TokenSymbol';
+import useBombStats from '../../hooks/useBombStats';
+import useLpStats from '../../hooks/useLpStats';
+import useLpStatsBTC from '../../hooks/useLpStatsBTC';
+import useModal from '../../hooks/useModal';
+import useZap from '../../hooks/useZap';
+import useBondStats from '../../hooks/useBondStats';
+import usebShareStats from '../../hooks/usebShareStats';
+import useTotalValueLocked from '../../hooks/useTotalValueLocked';
+import { Bomb as bombTesting, BShare as bShareTesting } from '../../bomb-finance/deployments/deployments.testing.json';
+import { Bomb as bombProd, BShare as bShareProd } from '../../bomb-finance/deployments/deployments.mainnet.json';
+import { roundAndFormatNumber } from '../../0x';
+import MetamaskFox from '../../assets/img/metamask-fox.svg';
+import { Box, Button, Card, CardContent, Grid, Paper } from '@material-ui/core';
+import ZapModal from '../Bank/components/ZapModal';
+import { Alert } from '@material-ui/lab';
+
+import { makeStyles } from '@material-ui/core/styles';
+import useBombFinance from '../../hooks/useBombFinance';
+import { ReactComponent as IconTelegram } from '../../assets/img/telegram.svg';
+import { Helmet } from 'react-helmet'
+import BombImage from '../../assets/img/bomb.png';
+
+import HomeImage from '../../assets/img/background.jpg';
+const BackgroundImage = createGlobalStyle`
+  body {
+    background: url(${HomeImage}) repeat !important;
+    background-size: cover !important;
+    background-color: #171923;
+  }
+`;
+const TITLE = 'bomb.money | BTC pegged algocoin'
+
+// const BackgroundImage = createGlobalStyle`
+//   body {
+//     background-color: grey;
+//     background-size: cover !important;
+//   }
+// `;
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    [theme.breakpoints.down('415')]: {
+      // marginTop: '10px'
+    },
+  },
+}));
+
+const Home = () => {
+  const classes = useStyles();
+  const TVL = useTotalValueLocked();
+  const bombFtmLpStats = useLpStatsBTC('BOMB-BTCB-LP');
+  const bShareFtmLpStats = useLpStats('BSHARE-BNB-LP');
+  const bombStats = useBombStats();
+  const bShareStats = usebShareStats();
+  const tBondStats = useBondStats();
+  const bombFinance = useBombFinance();
+
+  let bomb;
+  let bShare;
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    bomb = bombTesting;
+    bShare = bShareTesting;
+  } else {
+    bomb = bombProd;
+    bShare = bShareProd;
+  }
+
+  const buyBombAddress =
+    //  'https://pancakeswap.finance/swap?inputCurrency=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&outputCurrency=' +
+    'https://app.bogged.finance/bsc/swap?tokenIn=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&tokenOut=' +
+    bomb.address;
+  //https://pancakeswap.finance/swap?outputCurrency=0x531780FAcE85306877D7e1F05d713D1B50a37F7A';
+  const buyBShareAddress = 'https://app.bogged.finance/bsc/swap?tokenIn=BNB&tokenOut=0x531780FAcE85306877D7e1F05d713D1B50a37F7A';
+  const bombLPStats = useMemo(() => (bombFtmLpStats ? bombFtmLpStats : null), [bombFtmLpStats]);
+  const bshareLPStats = useMemo(() => (bShareFtmLpStats ? bShareFtmLpStats : null), [bShareFtmLpStats]);
+  const bombPriceInDollars = useMemo(
+    () => (bombStats ? Number(bombStats.priceInDollars).toFixed(2) : null),
+    [bombStats],
+  );
+  const bombPriceInBNB = useMemo(() => (bombStats ? Number(bombStats.tokenInFtm).toFixed(4) : null), [bombStats]);
+  const bombCirculatingSupply = useMemo(() => (bombStats ? String(bombStats.circulatingSupply) : null), [bombStats]);
+  const bombTotalSupply = useMemo(() => (bombStats ? String(bombStats.totalSupply) : null), [bombStats]);
+
+  const bSharePriceInDollars = useMemo(
+    () => (bShareStats ? Number(bShareStats.priceInDollars).toFixed(2) : null),
+    [bShareStats],
+  );
+  const bSharePriceInBNB = useMemo(
+    () => (bShareStats ? Number(bShareStats.tokenInFtm).toFixed(4) : null),
+    [bShareStats],
+  );
+  const bShareCirculatingSupply = useMemo(
+    () => (bShareStats ? String(bShareStats.circulatingSupply) : null),
+    [bShareStats],
+  );
+  const bShareTotalSupply = useMemo(() => (bShareStats ? String(bShareStats.totalSupply) : null), [bShareStats]);
+
+  const tBondPriceInDollars = useMemo(
+    () => (tBondStats ? Number(tBondStats.priceInDollars).toFixed(2) : null),
+    [tBondStats],
+  );
+  const tBondPriceInBNB = useMemo(() => (tBondStats ? Number(tBondStats.tokenInFtm).toFixed(4) : null), [tBondStats]);
+  const tBondCirculatingSupply = useMemo(
+    () => (tBondStats ? String(tBondStats.circulatingSupply) : null),
+    [tBondStats],
+  );
+  const tBondTotalSupply = useMemo(() => (tBondStats ? String(tBondStats.totalSupply) : null), [tBondStats]);
+
+  const bombLpZap = useZap({ depositTokenName: 'BOMB-BTCB-LP' });
+  const bshareLpZap = useZap({ depositTokenName: 'BSHARE-BNB-LP' });
+
+  const [onPresentBombZap, onDissmissBombZap] = useModal(
+    <ZapModal
+      decimals={18}
+      onConfirm={(zappingToken, tokenName, amount) => {
+        if (Number(amount) <= 0 || isNaN(Number(amount))) return;
+        bombLpZap.onZap(zappingToken, tokenName, amount);
+        onDissmissBombZap();
+      }}
+      tokenName={'BOMB-BTCB-LP'}
+    />,
+  );
+
+  const [onPresentBshareZap, onDissmissBshareZap] = useModal(
+    <ZapModal
+      decimals={18}
+      onConfirm={(zappingToken, tokenName, amount) => {
+        if (Number(amount) <= 0 || isNaN(Number(amount))) return;
+        bshareLpZap.onZap(zappingToken, tokenName, amount);
+        onDissmissBshareZap();
+      }}
+      tokenName={'BSHARE-BNB-LP'}
+    />,
+  );
+
+  return (
+    <Page>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <BackgroundImage />
+      <Grid container spacing={3}>
+        {/* Logo */}
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          style={{ display: 'flex', justifyContent: 'center', verticalAlign: 'middle', overflow: 'hidden' }}
+        >
+          <img src={BombImage} alt='Bomb.money' style={{ maxHeight: '240px' }} />
+        </Grid>
+        {/* Explanation text */}
+        <Grid item xs={12} sm={8}>
+          <Paper>
+            <Box p={4} style={{ textAlign: 'center' }}>
+              <h2>SHIELD FINANCE</h2>
+              <p>
+                $SHIELD is an algorithmic token pegged to AVAX
+              </p>
+              <p>
+                <strong>Introducing the backbone of a rapidly growing ecosystem aimed towards bringing liquidity and new use cases to the Avalanche network.</strong>
+                {/* Stake your BOMB-BTC LP in the Farm to earn BSHARE rewards. Then stake your earned BSHARE in the
+                Boardroom to earn more BOMB! */}
+              </p>
+              <p>
+                <IconTelegram alt="telegram" style={{ fill: '#dddfee', height: '15px' }} /> Join our{' '}
+                <a
+                  href="https://t.me/bombmoneybsc"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  style={{ color: '#dddfee' }}
+                >
+                  Telegram
+                </a>{' '}
+                to find out more!
+              </p>
+            </Box>
+          </Paper>
+        </Grid>
+
+      {/*   <Grid container spacing={3}>
+          <Grid item xs={12} sm={12} justify="center" style={{ margin: '12px', display: 'flex' }}>
+
+            <Alert variant="filled" severity="info">
+              <strong>Please Note:</strong> We were not impacted by any of the recent exploits. &nbsp;
+              <a rel="noopener noreferrer" href="https://medium.com/@bombbshare/dec-18-quick-update-regarding-grim-exploit-4f1a0c989fba" target="_blank">
+                Read more here.
+              </a>
+
+            </Alert>
+
+          </Grid>
+        </Grid>
+ */}
+        {/* TVL */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center">
+              <h2>Total Value Locked</h2>
+              <h4>$????</h4>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* Wallet */}
+        <Grid item xs={12} sm={8}>
+          <Card style={{ height: '100%' }}>
+            <CardContent align="center" style={{ marginTop: '2.5%' }}>
+              {/* <h2 style={{ marginBottom: '20px' }}>Wallet Balance</h2> */}
+              <Button href="/boardroom" className="shinyButton" style={{ margin: '10px' }}>
+                Stake Now
+              </Button>
+              <Button href="/farm" className="shinyButton" style={{ margin: '10px' }}>
+                Farm Now
+              </Button>
+              <Button
+                target="_blank"
+                href={buyBombAddress}
+                style={{ margin: '10px' }}
+                className={'shinyButton ' + classes.button}
+              >
+                Buy SHIELD
+              </Button>
+              <Button
+                target="_blank"
+                href={buyBShareAddress}
+                className={'shinyButton ' + classes.button}
+                style={{ marginLeft: '10px' }}
+              >
+                Buy SSHIELD
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* BOMB */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BOMB" />
+                </CardIcon>
+              </Box>
+              <Button
+                onClick={() => {
+                  bombFinance.watchAssetInMetamask('BOMB');
+                }}
+                style={{ position: 'absolute', top: '10px', right: '10px', border: '1px grey solid' }}
+              >
+                {' '}
+                <b>+</b>&nbsp;&nbsp;
+                <img alt="metamask fox" style={{ width: '20px', filter: 'grayscale(100%)' }} src={MetamaskFox} />
+              </Button>
+              <h2 style={{ marginBottom: '10px' }}>SHIELD</h2>
+          
+            
+              <Box>
+                <span style={{ fontSize: '16px', alignContent: 'flex-start' }}>
+                 $???? / SHIELD
+                </span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: $???? <br />
+                Circulating Supply: $????<br />
+                Total Supply: $????
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* BSHARE */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <Button
+                onClick={() => {
+                  bombFinance.watchAssetInMetamask('BSHARE');
+                }}
+                style={{ position: 'absolute', top: '10px', right: '10px', border: '1px grey solid' }}
+              >
+                {' '}
+                <b>+</b>&nbsp;&nbsp;
+                <img alt="metamask fox" style={{ width: '20px', filter: 'grayscale(100%)' }} src={MetamaskFox} />
+              </Button>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BSHARE" />
+                </CardIcon>
+              </Box>
+              <h2 style={{ marginBottom: '10px' }}>SSHARE</h2>
+
+              <Box>
+                <span style={{ fontSize: '16px' }}>$???? / SSHARE</span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: $????
+                <br />
+                Circulating Supply: ????<br />
+                Total Supply: ????
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* BBOND */}
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardContent align="center" style={{ position: 'relative' }}>
+              <Button
+                onClick={() => {
+                  bombFinance.watchAssetInMetamask('BBOND');
+                }}
+                style={{ position: 'absolute', top: '10px', right: '10px', border: '1px grey solid' }}
+              >
+                {' '}
+                <b>+</b>&nbsp;&nbsp;
+                <img alt="metamask fox" style={{ width: '20px', filter: 'grayscale(100%)' }} src={MetamaskFox} />
+              </Button>
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BBOND" />
+                </CardIcon>
+              </Box>
+              <h2 style={{ marginBottom: '10px' }}>SBOND</h2>
+              
+             
+              <Box>
+                <span style={{ fontSize: '16px' }}>$???? / SBOND</span>
+              </Box>
+              <span style={{ fontSize: '12px' }}>
+                Market Cap: $????<br />
+                Circulating Supply: ????<br />
+                Total Supply: ????
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card>
+            <CardContent align="center">
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BOMB-BTCB-LP" />
+                </CardIcon>
+              </Box>
+              <h2>BOMB-BTCB PancakeSwap LP</h2>
+              <Box mt={2}>
+                <Button disabled onClick={onPresentBombZap} className="shinyButtonDisabledSecondary">
+                  Zap In
+                </Button>
+              </Box>
+              <Box mt={2}>
+                <span style={{ fontSize: '26px' }}>
+                  {bombLPStats?.tokenAmount ? bombLPStats?.tokenAmount : '-.--'} BOMB /{' '}
+                  {bombLPStats?.ftmAmount ? bombLPStats?.ftmAmount : '-.--'} BTCB
+                </span>
+              </Box>
+              <Box>${bombLPStats?.priceOfOne ? bombLPStats.priceOfOne : '-.--'}</Box>
+              <span style={{ fontSize: '12px' }}>
+                Liquidity: ${bombLPStats?.totalLiquidity ? roundAndFormatNumber(bombLPStats.totalLiquidity, 2) : '-.--'}{' '}
+                <br />
+                Total Supply: {bombLPStats?.totalSupply ? roundAndFormatNumber(bombLPStats.totalSupply, 2) : '-.--'}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card>
+            <CardContent align="center">
+              <Box mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol="BSHARE-BNB-LP" />
+                </CardIcon>
+              </Box>
+              <h2>BSHARE-BNB PancakeSwap LP</h2>
+              <Box mt={2}>
+                <Button onClick={onPresentBshareZap} className="shinyButtonSecondary">
+                  Zap In
+                </Button>
+              </Box>
+              <Box mt={2}>
+                <span style={{ fontSize: '26px' }}>
+                  {bshareLPStats?.tokenAmount ? bshareLPStats?.tokenAmount : '-.--'} BSHARE /{' '}
+                  {bshareLPStats?.ftmAmount ? bshareLPStats?.ftmAmount : '-.--'} BNB
+                </span>
+              </Box>
+              <Box>${bshareLPStats?.priceOfOne ? bshareLPStats.priceOfOne : '-.--'}</Box>
+              <span style={{ fontSize: '12px' }}>
+                Liquidity: $
+                {bshareLPStats?.totalLiquidity ? roundAndFormatNumber(bshareLPStats.totalLiquidity, 2) : '-.--'}
+                <br />
+                Total Supply: {bshareLPStats?.totalSupply ? roundAndFormatNumber(bshareLPStats.totalSupply, 2) : '-.--'}
+              </span>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Page>
+  );
+};
+
+export default Home;
